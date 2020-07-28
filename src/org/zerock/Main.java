b2149518df4537057a30f5997744831bb7efbe1a
silenc3502 <gcccompil3r@gmail.com>
2020-07-28 15:52:25 +0900

    Initial commit

---
+++ src/org/zerock/Main.java
@@ -0,0 +1,11 @@
+package org.zerock;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String winner = SGPGame.doGame();
+
+        System.out.println("winner: " + winner);
+    }
+}
---
+++ src/org/zerock/SGPGame.java
@@ -0,0 +1,38 @@
+package org.zerock;
+
+import java.util.Scanner;
+
+public class SGPGame {
+
+
+
+    public static String doGame() {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("가위 0, 바위 1, 보 2 ");
+        int userValue = Integer.parseInt(scanner.nextLine()) % 3;
+
+        int comValue = (int)(Math.random() * 3);
+
+
+        if(comValue < userValue){
+            comValue += 3;
+        }
+
+        System.out.println("COM: " + comValue +" USER: " + userValue );
+
+        int gap = comValue - userValue;
+
+        if(gap == 1){
+            return "U";
+        }else if(gap == 2){
+            return "C";
+        }
+
+        return "D";
+
+    }
+
+}
